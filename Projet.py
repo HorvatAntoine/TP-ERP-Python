7927e48980f7afbcda1947a18bf9c1725a11dd9e
HorvatAntoine <horvatantoine@gmail.com>
2018-11-20 11:32:06 +0100

    NEW Ajout de la class Projet

---
+++ Projet.py
@@ -0,0 +1,7 @@
+class Projet :
+    def __init__(self, id, nom, nbJoursDev, nbJoursGestion, dateEmbauche):
+        self.id = id
+        self.nom = nom
+        self.nbJoursDev = nbJoursDev
+        self.nbJoursGestion = nbJoursGestion
+        self.dateEmbauche = dateEmbauche
